7ee2630c549e1c56d91fe5780cdaeef2bd137e6c
startdusk <522383345@qq.com>
2022-12-12 20:40:23 +0800

    feat✨:  rc

--- smart_pointers_and_interior_mutability/src/rc.rs
+++ smart_pointers_and_interior_mutability/src/rc.rs
@@ -1,3 +1,68 @@
-pub struct Rc<T> {
+use crate::cell::Cell;
+use std::marker::PhantomData;
+use std::ptr::NonNull;
+
+struct RcInner<T> {
     value: T,
+    refcount: Cell<usize>,
+}
+
+pub struct Rc<T> {
+    // NonNull 是对 裸指针的一种封装
+    inner: NonNull<RcInner<T>>,
+
+    // 加这一句主要是为了让编译器追踪 RcInner<T> 这个类型，因为NonNull是一个裸指针
+    // 所以rust并不知道这个T类型里面有哪些元素，drop的时候需要递归到底drop元素，裸指针没有类型信息
+    _marker: PhantomData<RcInner<T>>,
+}
+
+impl<T> Rc<T> {
+    pub fn new(v: T) -> Self {
+        let inner = Box::new(RcInner {
+            value: v,
+            refcount: Cell::new(1),
+        });
+
+        Rc {
+            // SAFETY: Box does not give us a null pointer.
+            inner: unsafe { NonNull::new_unchecked(Box::into_raw(inner)) },
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T> Clone for Rc<T> {
+    fn clone(&self) -> Self {
+        let inner = unsafe { self.inner.as_ref() };
+        let c = inner.refcount.get();
+        inner.refcount.set(c + 1);
+        Rc {
+            inner: self.inner,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T> std::ops::Deref for Rc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &unsafe { &*self.inner.as_ref() }.value
+    }
+}
+
+impl<T> Drop for Rc<T> {
+    fn drop(&mut self) {
+        let inner = unsafe { self.inner.as_ref() };
+        let c = inner.refcount.get();
+        if c == 1 {
+            drop(inner);
+            // SAFETY: we are the _only_ Rc leftm and we are being dropped.
+            // therefore, after us, there will be no Rc's, and no references to T.
+            let _ = unsafe { Box::from_raw(self.inner.as_ptr()) };
+        } else {
+            // there are other Rcs, so
+            inner.refcount.set(c - 1);
+        }
+    }
 }
